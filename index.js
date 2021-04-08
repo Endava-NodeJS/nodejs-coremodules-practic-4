a1eaaf9c7ece55c20689980083a050d9c9a4494c
vpogonea <vasile.pogonea@endava.com>
2021-04-08 15:45:30 +0300

    add get by id

--- index.js
+++ index.js
@@ -6,23 +6,36 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.setHeader('Content-Type', 'text/json');
-  if(req.url && req.url === '/notes') {
-    switch (req.method){
-      case 'GET':
-        const value = db.get()
-        res.write(JSON.stringify(value));
-        break;
-        case "POST":
-          req.on("data",(body)=>{
-              const data = body.toString();
-              const parsedData = JSON.parse(data)
-              db.add(parsedData)
-              res.write("Added Note with success")
-          })
-           break
-      default:
-        console.log('in default')
-    }
+  if(req.url && req.url.includes('notes')) {
+      if(req.url === '/notes') {
+          switch (req.method) {
+              case 'GET':
+                  const value = db.get()
+                  res.write(JSON.stringify(value));
+                  break;
+              case "POST":
+                  req.on("data",(body)=>{
+                      const data = body.toString();
+                      const parsedData = JSON.parse(data)
+                      db.add(parsedData)
+                      res.write("Added Note with success")
+                  })
+                  break
+              default:
+                  console.log('in strict default')
+          }
+      } else {
+          switch (req.method) {
+              case 'GET':
+                  const id = req.url.split('/')[req.url.split('/').length - 1]
+                  const value = db.get(id);
+                  res.write(JSON.stringify(value));
+                  break;
+              default:
+                  console.log('i non-strict default')
+          }
+      }
+
   }
   res.end();
 }
