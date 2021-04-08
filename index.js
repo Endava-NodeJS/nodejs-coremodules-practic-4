c0c9ee7064ceabbcbdafcdc1c538cec3e393fc6d
vpogonea <vasile.pogonea@endava.com>
2021-04-08 15:05:56 +0300

    add notes url

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const db = new DB('./notes.json');
 const http = require('http');
 
 const requestListener = function (req, res) {
-  const value = db.get()
-  res.writeHead(200);
-  res.end(JSON.stringify(value));
+  res.setHeader('Content-Type', 'text/json');
+  if(req.url && req.url === '/notes') {
+    switch (req.method){
+      case 'GET':
+        const value = db.get()
+        res.write(JSON.stringify(value));
+        break;
+      default:
+        console.log('in default')
+    }
+  }
+  res.end();
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(8089);
